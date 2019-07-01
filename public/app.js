487ca470b68118e87d1af4cb08511f0b05b22b9d
Waqar Habib <waqarhabib94@aggienetwork.com>
2019-06-30 23:16:41 -0500

    fixed paths so express serves css files

--- public/app.js
+++ public/app.js
@@ -1,3 +1,14 @@
 $('#addNote').on('click', function () {
     $("#note").modal("show");
   })
+
+// Grab the articles as a json
+$.getJSON("/articles", function(data) {
+  // For each one
+  for (var i = 0; i < data.length; i++) {
+    // Display the apropos information on the page
+    $("#title").append("<h2 itemid='" + data[i]._id + "'>");
+  }
+});
+
+//"<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].url + "</p>"
--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 // Dependencies
 var express = require("express");
 var mongoose = require("mongoose");
+var path = require('path');
 
 // Require axios and cheerio. This makes the scraping possible
 var axios = require("axios");
@@ -27,11 +28,11 @@ app.use(express.urlencoded({
 }));
 app.use(express.json());
 // Make public a static folder
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, '/public')));
 
-// Main route (simple Hello World Message)
+//Main route (simple Hello World Message)
 app.get("/", function (req, res) {
-  res.send("Hello world");
+  res.sendFile(__dirname + '/index.html');
 });
 
 app.get("/scrape", function (req, res) {
@@ -74,6 +75,25 @@ app.get("/articles", function(req, res) {
       res.json(err);
     });
 });
+
+// Route for grabbing a specific Article by id, populate it with it's note
+app.get("/articles/:id", function(req, res) {
+  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+  db.Article.findOne({ _id: req.params.id })
+    //console.log({ _id: req.params.id });
+    // ..and populate all of the notes associated with it
+    //.populate("note")
+    .then(function(dbArticle) {
+      // If we were able to successfully find an Article with the given id, send it back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+
 
 
 // Listen on port 5000

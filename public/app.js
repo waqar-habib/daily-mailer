753192351a5b49c1439011b75d17fcffee817d2b
Waqar Habib <waqarhabib94@aggienetwork.com>
2019-07-01 14:01:02 -0500

    notes show up in the console

--- public/app.js
+++ public/app.js
@@ -1,37 +1,32 @@
-// $(document).on('click', '#addNote', function () {
-//     $("#note").modal("show");
-// })
-
 // Grab the articles as a json
-$.getJSON("/articles", function(data) {
+$.getJSON("/articles", function (data) {
   // For each one
   console.log(data);
   var articles = [];
 
   for (var i = 0; i < data.length; i++) {
-    // Display the apropos information on the page
-    //$("#articles").append("<p data-id='" + data[i]._id + "'>");
+
     var articleBlurb = "";
     if (data[i].blurb) {
       articleBlurb = `<p class="post-subtitle" id="synopsis">${data[i].blurb}</p>`
     }
     var articleElement = `
-   <div class="row">
-    <div class="col-lg-8 col-md-10 mx-auto">
-      <div class="post-preview" id="articles">
-        <h2 class="post-title" id="title" data-id=${data[i]._id}>${data[i].title}</h2>
-        ${articleBlurb}
-        <p class="post-meta" id="url">${data[i].url}</p>
-        <button type="button" class="btn btn-dark" id="addNote" data-id=${data[i]._id}>Add Note</button>
-      </div>
-      <hr>
-    </div>
-  </div>`;
-  $(".article-container").append(articleElement);
+      <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+          <div class="post-preview" id="articles">
+            <h2 class="post-title" id="title" data-id=${data[i]._id}>${data[i].title}</h2>
+            ${articleBlurb}
+            <a class="post-meta" id="url" target="_blank" href="https://www.aljazeera.com${data[i].url}">Read Full Article <br><br></a>
+            <button type="button" class="btn btn-dark" id="addNote" data-id=${data[i]._id}>Add Note</button>
+          </div>
+          <hr>
+        </div>
+      </div>`;
+    $(".article-container").append(articleElement);
   }
 });
 
-$(document).on("click", "#addNote", function() {
+$(document).on("click", "#addNote", function () {
   // Empty the notes from the note section
   $("#noteBody").val("");
   // Save the id from the p tag
@@ -39,24 +34,43 @@ $(document).on("click", "#addNote", function() {
 
   // Now make an ajax call for the Article
   $.ajax({
-    method: "GET",
-    url: "/articles/" + thisId
-  })
+      method: "GET",
+      url: "/articles/" + thisId
+    })
     // With that done, add the note information to the page
-    .then(function(data) {
-      console.log(data);
+    .then(function (data) {
+      //console.log(data);
       if (data.note) {
-        $("#noteBody").val(data.note);        
+        $("#noteBody").append(data.note);        
       }
       // A button to submit a new note, with the id of the article saved to it
       $(".saveNote").data("id", thisId)
       $("#note").modal("show");
-      
+
     });
 });
 
 $(document).on("click", ".saveNote", function () {
-  console.log($("#noteBody").val());
-  console.log($(this).data("id"));
-  $("#note").modal("hide");
-})
+  // Grab the id associated with the article from the submit button
+  var thisId = $(".saveNote").data("id");
+  console.log(thisId);
+  var noteBody = $("#noteBody").val();
+  console.log(noteBody);
+  //Run a POST request to change the note, using what's entered in the inputs
+  $.ajax({
+      method: "POST",
+      url: "/articles/" + thisId,
+      data: {
+        // Value taken from noteBody
+        body: noteBody
+      }
+    })
+    // // With that done
+    .then(function (data) {
+      // Log the response
+      console.log(data);
+      $("#noteBody").empty();
+      $("#note").modal("hide");
+    });
+});
+

c4340b72ce07a941169ce79eb41c584018e1262e
Waqar Habib <waqarhabib94@aggienetwork.com>
2019-07-01 12:43:53 -0500

    got the data from db to append to front end

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-// $('#addNote').on('click', function () {
+// $(document).on('click', '#addNote', function () {
 //     $("#note").modal("show");
 // })
 
@@ -11,8 +11,52 @@ $.getJSON("/articles", function(data) {
   for (var i = 0; i < data.length; i++) {
     // Display the apropos information on the page
     //$("#articles").append("<p data-id='" + data[i]._id + "'>");
-    console.log(data[i]._id);
+    var articleBlurb = "";
+    if (data[i].blurb) {
+      articleBlurb = `<p class="post-subtitle" id="synopsis">${data[i].blurb}</p>`
+    }
+    var articleElement = `
+   <div class="row">
+    <div class="col-lg-8 col-md-10 mx-auto">
+      <div class="post-preview" id="articles">
+        <h2 class="post-title" id="title" data-id=${data[i]._id}>${data[i].title}</h2>
+        ${articleBlurb}
+        <p class="post-meta" id="url">${data[i].url}</p>
+        <button type="button" class="btn btn-dark" id="addNote" data-id=${data[i]._id}>Add Note</button>
+      </div>
+      <hr>
+    </div>
+  </div>`;
+  $(".article-container").append(articleElement);
   }
 });
 
-// + data[i].title + "<br />" + data[i].url + "</p>"
+$(document).on("click", "#addNote", function() {
+  // Empty the notes from the note section
+  $("#noteBody").val("");
+  // Save the id from the p tag
+  var thisId = $(this).attr("data-id");
+
+  // Now make an ajax call for the Article
+  $.ajax({
+    method: "GET",
+    url: "/articles/" + thisId
+  })
+    // With that done, add the note information to the page
+    .then(function(data) {
+      console.log(data);
+      if (data.note) {
+        $("#noteBody").val(data.note);        
+      }
+      // A button to submit a new note, with the id of the article saved to it
+      $(".saveNote").data("id", thisId)
+      $("#note").modal("show");
+      
+    });
+});
+
+$(document).on("click", ".saveNote", function () {
+  console.log($("#noteBody").val());
+  console.log($(this).data("id"));
+  $("#note").modal("hide");
+})
